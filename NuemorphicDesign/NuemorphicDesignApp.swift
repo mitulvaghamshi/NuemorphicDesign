2e36e1ec6342ba4bcbe7d833ec2e8fe9e0ac21cb
mitulvaghamshi <mitulvaghmashi@gmail.com>
2022-11-20 04:28:05 -0500

    Initial commit

---
+++ NuemorphicDesign/NuemorphicDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  NuemorphicDesignApp.swift
+//  NuemorphicDesign
+//
+//  Created by Mitul Vaghamshi on 2022-11-20.
+//
+
+import SwiftUI
+
+@main
+struct NuemorphicDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Nuemorphic.swiftpm
+ SwiftUI: Showcase how to create a Nuemorphic Design for entire app - (Source: Online Resources)
+
+## Preview
+
+![Preview](screenshot.png)
